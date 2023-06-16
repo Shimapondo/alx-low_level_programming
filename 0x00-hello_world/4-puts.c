d5fd35772120a16037bf298769cd1ffec407b4f9
Shimapondo <williamnamabende@gmail.com>
2023-06-16 02:18:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - demonstrates the usage of the write() function.
+ * @void: no parameters accepted
+ * It prints a string to the console
+ * Return: 0 Program excuted succesfully.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
