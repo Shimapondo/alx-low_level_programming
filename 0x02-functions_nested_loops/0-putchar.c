6e182f657e7b2413024648f0a6241d165d07c771
Shimapondo <williamnamabende@gmail.com>
2023-06-21 15:10:35 -0700

    0-putchar commit 3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,14 +10,13 @@
 
 int main(void)
 {
-	char text[9] = "_putchar";
+	char text[10] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 9; i++)
 	{
 		_putchar(text[i]);
 	}
-	_putchar('\n');
 
 	return (0);
 }
