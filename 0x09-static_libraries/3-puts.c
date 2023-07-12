aef3c47d47ff15666e405257cd63c78e70c9f037
Shimapondo <williamnamabende@gmail.com>
2023-07-12 15:53:29 -0700

    My first static Library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * puts : prints on display
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
