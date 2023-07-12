287eaeb36ebef99aa536e706c0b6536915aeadb3
Shimapondo <williamnamabende@gmail.com>
2023-07-12 16:50:05 -0700

    Static library libmy.a containing all the functions listed

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints variable
+ *
+ * _puts : prints words on display
+ *
+ * Return;  returns 0(success)
+ */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
